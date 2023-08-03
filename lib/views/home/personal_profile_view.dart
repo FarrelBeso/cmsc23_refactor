c7b92c83217c76adb1fd605567d80ba170326a61
FarrelBeso <vabeso@up.edu.ph>
2023-08-03 20:25:42 +0800

    update: fixing on profile page to use providers

--- lib/views/home/personal_profile_view.dart
+++ lib/views/home/personal_profile_view.dart
@@ -1,76 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_refactor/provider/auth_provider.dart';
 
 class PersonalProfileView extends StatelessWidget {
   const PersonalProfileView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // return Consumer<AuthProvider>(
-    //   builder: (context, provider, child) {
-    return Expanded(
-      child: Container(
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              color: Theme.of(context).primaryColor,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 24,
-                  ),
-                  Text(
-                    //'${provider.currentuser?.firstName} ${provider.currentuser?.lastName}',
-                    'XXXXXXX',
-                    style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                  ),
-                  Text(
-                    'USERNAME', // provider.currentuser?.username ?? 'N/A',
-                    style: TextStyle(fontSize: 16, color: Colors.white70),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: Container(
-                          padding: EdgeInsets.all(8),
-                          child: Text(
-                            'PROFILE',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: Colors.white),
+    return Consumer<AuthProvider>(builder: (context, provider, child) {
+      return Expanded(
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                color: Theme.of(context).primaryColor,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 24,
+                    ),
+                    Text(
+                      '${provider.currentuser?.firstName} ${provider.currentuser?.lastName}',
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    Text(
+                      provider.currentuser?.username ?? 'N/A',
+                      style: TextStyle(fontSize: 16, color: Colors.white70),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Container(
+                            padding: EdgeInsets.all(8),
+                            child: Text(
+                              'PROFILE',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.white),
+                            ),
                           ),
                         ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          padding: EdgeInsets.all(8),
-                          child: Text('FRIENDS',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(color: Colors.white)),
-                        ),
-                      )
-                    ],
-                  ),
-                ],
+                        Expanded(
+                          child: Container(
+                            padding: EdgeInsets.all(8),
+                            child: Text('FRIENDS',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(color: Colors.white)),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
 
-            // put whatever the current tab is
-            ProfileSection()
-            //FriendsSection()
-          ],
+              // put whatever the current tab is
+              ProfileSection()
+              //FriendsSection()
+            ],
+          ),
         ),
-      ),
-    );
-    // });
+      );
+    });
   }
 }
 
@@ -80,57 +80,57 @@ class ProfileSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // return Consumer<AuthProvider>(
-    //   builder: (context, provider, child) {
-    return Container(
-      padding: EdgeInsets.all(8),
-      child: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.all(6),
-            child: Row(
-              children: [
-                Container(padding: EdgeInsets.all(8), child: Text('ID: ')),
-                Text('N/A'), //provider.currentuser?.id ?? 'N/A')
-              ],
-            ),
-          ),
-          Card(
-            child: Container(
-              padding: EdgeInsets.all(12),
-              child: Row(
-                children: [
-                  Container(
-                      padding: EdgeInsets.all(8), child: Icon(Icons.cake)),
-                  Text(DateFormat(DateFormat.YEAR_MONTH_DAY).format(DateTime(
-                      1900))) //provider.currentuser?.birthday ?? DateTime(1900)))
-                ],
+    return Consumer<AuthProvider>(
+      builder: (context, provider, child) {
+        return Container(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(6),
+                child: Row(
+                  children: [
+                    Container(padding: EdgeInsets.all(8), child: Text('ID: ')),
+                    Text(provider.currentuser?.id ?? 'N/A')
+                  ],
+                ),
               ),
-            ),
-          ),
-          Card(
-            child: Container(
-              padding: EdgeInsets.all(12),
-              child: Row(
-                children: [
-                  Container(
-                      padding: EdgeInsets.all(8),
-                      child: Icon(Icons.location_on)),
-                  Text('N/A'), //provider.currentuser?.location ?? 'N/A')
-                ],
+              Card(
+                child: Container(
+                  padding: EdgeInsets.all(12),
+                  child: Row(
+                    children: [
+                      Container(
+                          padding: EdgeInsets.all(8), child: Icon(Icons.cake)),
+                      Text(DateFormat(DateFormat.YEAR_MONTH_DAY).format(
+                          provider.currentuser?.birthday ?? DateTime(1900)))
+                    ],
+                  ),
+                ),
               ),
-            ),
+              Card(
+                child: Container(
+                  padding: EdgeInsets.all(12),
+                  child: Row(
+                    children: [
+                      Container(
+                          padding: EdgeInsets.all(8),
+                          child: Icon(Icons.location_on)),
+                      Text(provider.currentuser?.location ?? 'N/A')
+                    ],
+                  ),
+                ),
+              ),
+              Divider(),
+              Container(
+                  padding: EdgeInsets.all(8),
+                  alignment: Alignment.topLeft,
+                  child: Text('Sample Biography. Lorem Ipsum dolor sit amet.'))
+            ],
           ),
-          Divider(),
-          Container(
-              padding: EdgeInsets.all(8),
-              alignment: Alignment.topLeft,
-              child: Text('Sample Biography. Lorem Ipsum dolor sit amet.'))
-        ],
-      ),
+        );
+      },
     );
-    //   },
-    // );
   }
 }
 
