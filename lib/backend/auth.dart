69c52afc4b1efbcd742bcc87139b455c5c325a65
FarrelBeso <vabeso@up.edu.ph>
2023-08-03 20:06:34 +0800

    update: set the sign in auth api

--- lib/backend/auth.dart
+++ lib/backend/auth.dart
@@ -1,14 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:todo_refactor/model/user_model.dart';
 
 class AuthAPI {
+  final db = FirebaseFirestore.instance;
   User? get currentUser => FirebaseAuth.instance.currentUser;
 
   Stream<User?> get authStateChanges =>
       FirebaseAuth.instance.authStateChanges();
 
-  Future<void> signIn(String email, String password) async {
-    await FirebaseAuth.instance
-        .createUserWithEmailAndPassword(email: email, password: password);
+  Future<void> signIn(UserModel usermodel, String password) async {
+    final result = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: usermodel.email!, password: password);
+    usermodel.id = result.user!.uid; // set the user here
+    await addToDatabase(usermodel);
   }
 
   Future<void> login(String email, String password) async {
@@ -18,5 +23,16 @@ class AuthAPI {
 
   Future<void> signOut() async {
     await FirebaseAuth.instance.signOut();
+  }
+
+  // database related actions
+  Future<void> addToDatabase(UserModel usermodel) async {
+    final docRef = db
+        .collection("users")
+        .withConverter(
+            fromFirestore: UserModel.fromFirestore,
+            toFirestore: (UserModel model, options) => model.toFirestore())
+        .doc(usermodel.id);
+    await docRef.set(usermodel);
   }
 }
