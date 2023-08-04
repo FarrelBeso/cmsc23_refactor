72755f02818cf971808eb40bbf97319b925fc1db
FarrelBeso <vabeso@up.edu.ph>
2023-08-05 03:26:52 +0800

    update: added code for getting the task list

--- lib/backend/tasks_api.dart
+++ lib/backend/tasks_api.dart
@@ -14,4 +14,30 @@ class TasksAPI {
         .doc(taskmodel.id);
     await docRef.set(taskmodel);
   }
+
+  // fetch task ids of the given user
+  Future<List<String>> getTaskIdsFromUser(String id) async {
+    List<String> taskIds = [];
+    db
+        .collection("tasks")
+        .where("ownerId",
+            isEqualTo: id) // should be adjusted to include friends
+        .get()
+        .then((querySnapshot) {
+      for (var docSnapshot in querySnapshot.docs) {
+        taskIds.add(docSnapshot.id);
+      }
+    });
+    return taskIds;
+  }
+
+  // fetch info about a particular task id
+  Future<TaskModel?> getTaskInfo(String id) async {
+    final docRef = db.collection("tasks").doc(id).withConverter(
+        fromFirestore: TaskModel.fromFirestore,
+        toFirestore: (TaskModel model, _) => model.toFirestore());
+    final docSnap = await docRef.get();
+    final taskmodel = docSnap.data();
+    return taskmodel;
+  }
 }
--- lib/provider/task_provider.dart
+++ lib/provider/task_provider.dart
@@ -17,4 +17,28 @@ class TaskProvider extends ChangeNotifier {
     });
     return response;
   }
+
+  // get list of tasks
+  Future<ResponseModel> getTaskList() async {
+    ResponseModel response = ResponseModel(success: true); // default
+    List<TaskModel> tasklist = [];
+    await TasksAPI()
+        .getTaskIdsFromUser(AuthAPI().currentUser!.uid)
+        .then((taskIds) async {
+      for (var id in taskIds) {
+        TaskModel? task = await TasksAPI().getTaskInfo(id);
+        if (task == null) {
+          // set it up
+          response = ResponseModel(
+              success: false, message: 'Failed to fetch all tasks');
+        } else {
+          tasklist.add(task);
+        }
+      }
+    }).onError((error, stackTrace) {
+      print(error);
+    });
+    response.content = tasklist;
+    return response;
+  }
 }
